fe440ac8c1e879a7f89486c72b8c3d5ff7b0befa
Fluke <dr.catth@gmail.com>
2024-02-27 12:47:51 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdlib>// Include for ato
 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        cout << "Please input numbers to find average.";
+    } else {
+        double sum = 0;
+        for (int i = 1; i < argc; i++) {
+            sum += atof(argv[i]);
+        }
+        cout << "---------------------------------" << '\n';
+        cout << "Average of " << argc - 1 << " numbers = " << sum / (argc - 1) << '\n';
+        cout << "---------------------------------" << '\n';
+    }
+
+    return 0;
+}
